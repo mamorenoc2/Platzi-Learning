92b3740a5f926771311969da9d8b7e2d18209540
Manuela Moreno <49201450+mamorenoc2@users.noreply.github.com>
2023-06-29 14:09:03 -0500

    Full stack javascript

---
+++ Desarrollo Web/Full Stack Developer con JavaScript/Curso Basico de JavaScript/ejercicios_clase/funciones.js
@@ -0,0 +1,20 @@
+// Declarativas
+
+function miFuncion(){
+    return 3;
+}
+
+//Expresion
+
+var miFuncion2 = function(a, b){
+    return a + b;
+}
+
+miFuncion2(2, 4);
+
+
+function saludarEstudiante(estudiante){
+    console.log(`Hola ${estudiante}!`)
+}
+
+saludarEstudiante('Manuela')
--- Desarrollo Web/Full Stack Developer con JavaScript/Curso Basico de JavaScript/variables.js
+++ Desarrollo Web/Full Stack Developer con JavaScript/Curso Basico de JavaScript/variables.js
@@ -1,52 +1,71 @@
+/*-----------  FUNCIONES EN JAVASCRIPT---------*/
 
-4 + "7"; // 47
-4 * "7"; // 28
-2 + true; // 3
-false - 3; // -3
-
-/**
- * Dentro de JavaScript tenemos tres formas de declarar una variable las cuales son: var, const y let.
-
-Var: Era la forma en que se declaraban las variables hasta ECMAScript 5. 
-Casi ya no se usa porque es de forma global y tiene las siguientes características:
-
-*** Se puede reinicializar: osea todas las variables se inicializan, por ejemplo:
-Var pokemonType = ‘electric’ entonces reinicializar es:
-Var pokemonType = ‘grass’ osea la misma variable con diferentes datos el último dato predomina.
-
-*** Se puede reasignar: osea la variable ya inicializada le reasignamos otro valor por ejemplo: inicializamos la variable: Var pokemonType = ‘electric’ ahora la reasignamos pokemonType = ‘grass’ ya no va var
-
-*** Su alcance es función global: osea inicializamos la variable, pero la podemos llamar desde cualquier bloque (una llave abierta y una cerrada {}) pero hay que tener mucho cuidado con ello ya que puede haber peligro, no es recomendable usar VAR.
-
-*/
-var nombre = 'manuela';
-
-var x = 5;
-console.log(x); // 5
-
-{
-    var y = 10;
+// Declaración
+function suma (a,b){
+    return a + b
 }
-console.log(y); // 10
-
-
-
-/*
-const y let es la forma en que se declaran las variables a partir de ECMAScript 6,
-
+/* 
+function nombre (parámetros) {
+    contenido
+    return valor
+} 
 */
 
-/* const: sirve para declarar variables que nunca van a ser modificadas:
 
-**** No se puede reinicilizar: es una const única no puede haber otra inicializada con el mismo nombre. const pokemonType = ‘electric’ no puede haber:
-const pokemonType = ‘grass’
-o No se pude re asignar: una vez que la hayamos inicializado no la podemos reasignar solo con su nombre: const pokemonType = ‘electric’ no puede ejecutarse:
-pokemonType = ‘grass’
-o No es inmutable: osea no puede cambiar con objetos:
+// Invocación
+suma(2,3)
+// nombre(argumentos)
 
-Let: Son variables que pueden ser modificadas, se pueden cambiar:
-o No se puede reinicilizar: es una const única no puede haber otra inicializada con el mismo nombre. let pokemonType = ‘electric’ no puede haber:
-let pokemonType = ‘grass’
-o Se puede reasignar: Osea la variable ya inicializada le reasignamos otro valor por ejemplo: inicializamos la variable: let pokemonType = ‘electric’ ahora la reasignamos pokemonType = ‘grass’
-o Su contexto de es bloque: Solo funciona dentro de un bloque {}, fuera de ello no.
- */
+var resultado1 = suma(2,3)
+var resultado2 = suma(4,6)
+var resultado3 = suma(10,12)
+
+console.log(resultado1) //5
+console.log(resultado2) //10
+console.log(resultado3) //22
+
+
+
+// Declaración
+function saludar(nombre){
+    console.log("Hola " + nombre) 
+}
+// Invocaciones
+saludar("JavaScript") //"Hola JavaScript"
+saludar("Platzi") // "Hola Platzi"
+
+
+
+console.log(`Hola ${nombre}`)
+
+
+// Declaración
+var suma = function (a, b) {
+    return a + b
+  }
+  // Invocación
+  var resultado = suma(2, 2)
+  
+  console.log(resultado) //4
+
+//Scope global
+var nombre = "JavaScript"
+
+function saludar(){
+    console.log("Hola " + nombre)
+}
+
+saludar()
+
+//Scope local
+function saludo() {
+    let nombre = "Andres"
+    console.log(nombre)
+}
+
+saludo() // "Andres"
+console.log(nombre) 
+// ReferenceError: nombre is not defined
+
+
+
