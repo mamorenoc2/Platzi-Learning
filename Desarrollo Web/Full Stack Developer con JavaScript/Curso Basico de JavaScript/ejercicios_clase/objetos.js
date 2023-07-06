4516ecdc370a21b305330c183387d083033972be
Manuela Moreno <49201450+mamorenoc2@users.noreply.github.com>
2023-07-06 09:12:16 -0500

    Objetos en javaScript

---
+++ Desarrollo Web/Full Stack Developer con JavaScript/Curso Basico de JavaScript/ejercicios_clase/objetos.js
@@ -0,0 +1,10 @@
+var miAuto = {
+    marca: 'Toyota',
+    modelo: 'Corolla',
+    annio: 2020,
+    detalleDelAuto : function() {
+        console.log(`Auto ${this.modelo} ${this.annio}`);
+    }
+};
+
+console.log(miAuto.detalleDelAuto());
