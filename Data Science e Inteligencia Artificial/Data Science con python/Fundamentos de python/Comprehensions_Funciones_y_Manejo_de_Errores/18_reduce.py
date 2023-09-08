bde72e27283bf0f85c172e03fc3e84c25d5ca922
Manuela Moreno <49201450+mamorenoc2@users.noreply.github.com>
2023-09-08 13:05:55 -0500

    Reduce function in python practice

--- Data Science e Inteligencia Artificial/Data Science con python/Fundamentos de python/Comprehensions_Funciones_y_Manejo_de_Errores/18_reduce.py
+++ Data Science e Inteligencia Artificial/Data Science con python/Fundamentos de python/Comprehensions_Funciones_y_Manejo_de_Errores/18_reduce.py
@@ -2,6 +2,11 @@ import functools
 
 numbers = [1, 2, 3, 4]
 
+def acumulador(counter, item):
+    print(counter)
+    print(item)
+    return counter + item
+
 result = functools.reduce(lambda counter, item: counter + item, numbers)
 
 print(result)
